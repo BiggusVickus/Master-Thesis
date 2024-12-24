faba5a538cd894327c27cbc6953ca21ed5a6c0f6
Victor Piaskowski <victor.piaskowski@proton.me>
2024-12-24 16:03:30 +0100

    bacteria, environment, phages

---
+++ Classes/Bacteria.py
@@ -0,0 +1,7 @@
+import Microorganism
+
+class Bacteria(Microorganism):
+    def __init__(self):
+        super().__init__
+        self.type = "Bacteria"
+    
---
+++ Classes/Phage.py
@@ -0,0 +1,6 @@
+import Microorganism
+
+class Phage(Microorganism):
+    def __init__(self):
+        super().__init__
+        self.type = "Phage"
