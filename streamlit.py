bae35ea1681f60cc3c37da1ef2c9e53dd5c1b83b
Victor Piaskowski <victor.piaskowski@proton.me>
2025-02-28 02:15:20 +0100

    remove old vis code, use new visualizer class

--- streamlit.py
+++ streamlit.py
@@ -87,11 +87,12 @@ class System(Analysis):
         flattened_y1 = self.flatten_lists_and_matrices(new_N, new_U, new_I, new_P)
         return flattened_y1
 
+
 # graph = GraphMakerGUI()
 # graph.export_graph('simple_test.gexf')
+# graph = System('simple_test.gexf')
 
-graph = System('simple_test.gexf')
-# graph = System('example.gexf')
+graph = System('example.gexf')
 phage_nodes = graph.get_nodes_of_type('P')
 bacteria_nodes = graph.get_nodes_of_type('B')
 resource_nodes = graph.get_nodes_of_type('R')
@@ -103,106 +104,25 @@ I0 = graph.initialize_new_matrix(len(U0), graph.M)
 P0 = graph.initialize_new_parameter_from_node(phage_nodes, "Initial_Population")
 
 e_vector = graph.initialize_new_parameter_from_node(resource_nodes, 'e')
+tau_vector = graph.initialize_new_parameter_from_node(bacteria_nodes, 'tau')
 v_matrix = graph.initialize_new_parameter_from_edges(bacteria_nodes, resource_nodes, 'v')
 K_matrix = graph.initialize_new_parameter_from_edges(bacteria_nodes, resource_nodes, 'K')
 r_matrix = graph.initialize_new_parameter_from_edges(phage_nodes, bacteria_nodes, 'r')
-tau_vector = graph.initialize_new_parameter_from_node(bacteria_nodes, 'tau')
 B_matrix = graph.initialize_new_parameter_from_edges(phage_nodes, bacteria_nodes, 'Burst_Size')
 
-graph_data = {"R0": pd.DataFrame(R0, columns=["R0 0"]), "U0": pd.DataFrame(U0, columns=["U0 0"]), "I0": pd.DataFrame(I0, columns=["I0 0", "I0 1", "I0 2", "I0 3"]), "P0": pd.DataFrame(P0, columns=["P0 0"])}
-non_graph_data_vector = {"e_vector": pd.DataFrame(e_vector, columns=["e_vector 0"]), "tau_vector": pd.DataFrame(tau_vector, columns=["tau_vector 0"])}
-non_graph_data_matrix = {"v_matrix": pd.DataFrame(v_matrix, columns=["v_matrix 0"]), "K_matrix": pd.DataFrame(K_matrix, columns=["K_matrix 0"]), "r_matrix": pd.DataFrame(r_matrix, columns=["r_matrix 0"]), "B_matrix": pd.DataFrame(B_matrix, columns=["B_matrix 0"])}
+visualizer = Visualizer(graph)
+visualizer.add_graph_data("Resources", R0, resource_nodes)
+visualizer.add_graph_data("Uninfected Bacteria", U0, bacteria_nodes)
+visualizer.add_graph_data("Infected Bacteria", I0, row_names=bacteria_nodes, column_names=[f"Step {i}" for i in range(int(graph.M))], add_columns=4)
+visualizer.add_graph_data("Phages", P0 , phage_nodes)
 
-# graph_data = {"R0": pd.DataFrame(R0, columns=["R0 0"]), 
-#     "U0": pd.DataFrame(U0, columns=["U0 0"]), "I0": 
-#               pd.DataFrame(I0, columns=["I0 0", "I0 1", "I0 2", "I0 3"]), 
-#               "P0": pd.DataFrame(P0, columns=["P0 0"])}
-# non_graph_data_vector = {"e_vector": pd.DataFrame(e_vector, columns=["e_vector 0"]), "tau_vector": pd.DataFrame(tau_vector, columns=["tau_vector 0"])}
-# non_graph_data_matrix = {"v_matrix": pd.DataFrame(v_matrix, columns=["v_matrix 0", "v_matrix 1", "v_matrix 2"]), 
-#                          "K_matrix": pd.DataFrame(K_matrix, columns=["K_matrix 0", "K_matrix 1", "K_matrix 2"]), 
-#                          "r_matrix": pd.DataFrame(r_matrix, columns=["r_matrix 0", "r_matrix 1", "r_matrix 2"]), 
-#                          "B_matrix": pd.DataFrame(B_matrix, columns=["B_matrix 0", "B_matrix 1", "B_matrix 2"])}
+visualizer.add_non_graph_data_vector("e_vector", e_vector, resource_nodes)
+visualizer.add_non_graph_data_vector("tau_vector", tau_vector, bacteria_nodes)
+visualizer.add_non_graph_data_matrix("v_matrix", v_matrix, bacteria_nodes, resource_nodes)
+visualizer.add_non_graph_data_matrix("K_matrix", K_matrix, bacteria_nodes, resource_nodes)
+visualizer.add_non_graph_data_matrix("r_matrix", r_matrix, phage_nodes, bacteria_nodes)
+visualizer.add_non_graph_data_matrix("B_matrix", B_matrix, phage_nodes, bacteria_nodes)
 
-app = Dash()
-app.layout = [
-    html.H1("Line Chart of N, U, I, and P"),
-    html.Button('Save and rerun model', id='submit-matrices'),
-    *[
-        dcc.Graph(id={"type": "plotting-graph-data", "index": "R"}),
-        dcc.Graph(id={"type": "plotting-graph-data", "index": "U"}),
-        dcc.Graph(id={"type": "plotting-graph-data", "index": "I"}),
-        dcc.Graph(id={"type": "plotting-graph-data", "index": "P"}),
-    ],
-    html.H1("Graphing Data"),
-    *[
-        html.Div([
-            html.H2(f"DataTable for {name}"),
-            dash_table.DataTable(
-                table.to_dict('records'),
-                id={"type":'edit-graphing-data', 'index': name},
-                columns=[{'name': f"{col}", 'id': col} for col in table.columns],
-                editable=True
-            ),
-        ]) for name, table in graph_data.items()
-    ], 
-    html.H1("Non Graphing Data: Vectors"),
-    *[
-        html.Div([
-            html.H2(f"DataTable for {name}"),
-            dash_table.DataTable(
-                table.to_dict('records'),
-                id={"type":'edit-non-graphing-data-vectors', 'index': name},
-                columns=[{'name': f"{col}", 'id': col} for col in table.columns],
-                editable=True
-            ),
-        ]) for name, table in non_graph_data_vector.items()
-    ], 
-    html.H1("Non Graphing Data: Matrices"),
-    *[
-        html.Div([
-            html.H2(f"DataTable for {name}"),
-            dash_table.DataTable(
-                table.to_dict('records'),
-                id={"type":'edit-non-graphing-data-matrices', 'index': name},
-                columns=[{'name': f"{col}", 'id': col} for col in table.columns],
-                editable=True
-            ),
-        ]) for name, table in non_graph_data_matrix.items()
-    ]
-]
+visualizer.add_other_parameters(phage_nodes, bacteria_nodes, resource_nodes, int(graph.M))
 
-
-@callback(
-    [Output({"type": "plotting-graph-data", "index": "R"}, "figure"),
-    Output({"type": "plotting-graph-data", "index": "U"}, "figure"),
-    Output({"type": "plotting-graph-data", "index": "I"}, "figure"),
-    Output({"type": "plotting-graph-data", "index": "P"}, "figure")],
-    Input('submit-matrices', 'n_clicks'),
-    State({'type': 'edit-graphing-data', 'index': ALL}, 'data'),
-    State({'type': 'edit-non-graphing-data-vectors', 'index': ALL}, 'data'),
-    State({'type': 'edit-non-graphing-data-matrices', 'index': ALL}, 'data'), prevent_initial_call=True
-)
-def rerun_matrices(n_clicks, graphing_data, graphing_data_vectors, graphing_data_matrices):
-    new_graphing_data = [pd.DataFrame.from_dict(data_values).astype(float).to_numpy() for data_values in graphing_data]
-    flattened = graph.flatten_lists_and_matrices(*new_graphing_data)
-    new_non_graphing_data_vectors = [pd.DataFrame.from_dict(data_values).astype(float).to_numpy().T[0] for data_values in graphing_data_vectors]
-    new_non_graphing_data_matrices = [pd.DataFrame.from_dict(data_values).astype(float).to_numpy() for data_values in graphing_data_matrices]
-    new_updated_data = graph.solve_system(graph.new_system2, flattened, graph, phage_nodes, bacteria_nodes, resource_nodes, *new_non_graphing_data_vectors, *new_non_graphing_data_matrices, int(graph.M))
-    solved_y = new_updated_data.y
-    new_N, new_U, new_I, new_P = graph.unflatten_initial_matrix(solved_y, [len(R0), len(U0), 4, len(P0)])
-    new_I = [new_I[0] + new_I[1] + new_I[2] + new_I[3]]
-    fig1 = go.Figure(dict(text="Nutrients"))
-    for i in range(len(new_N)):
-        fig1.add_trace(go.Scatter(x=new_updated_data.t, y=new_N[i], mode="lines", name="Updated Line"))
-    fig2 = go.Figure(dict(text="Uninfected Bacteria"))
-    for i in range(len(new_U)):
-        fig2.add_trace(go.Scatter(x=new_updated_data.t, y=new_U[i], mode="lines", name="Updated Line"))
-    fig3 = go.Figure(dict(text="Infected Bacteria"))
-    for i in range(len(new_I)):
-        fig3.add_trace(go.Scatter(x=new_updated_data.t, y=new_I[i], mode="lines", name="Updated Line"))
-    fig4 = go.Figure(dict(text="Phages"))
-    for i in range(len(new_P)):
-        fig4.add_trace(go.Scatter(x=new_updated_data.t, y=new_P[i], mode="lines", name="Updated Line"))
-    return [fig1, fig2, fig3, fig4]
-
-app.run(debug=True, use_reloader=True)
+visualizer.run()
